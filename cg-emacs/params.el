7ddcbb168c70053cc3d5f6847f0c8b8071ca862a
rjcordoba <rcordoba77@yahoo.es>
2021-07-11 19:10:56 +0200

    Eliminado archivo params.

--- cg-emacs/params.el
+++
@@ -1,7 +0,0 @@
-;------------------------------------------------------------------
-;    Archivo de configuración de Emacs - R. Córdoba García
-;    Parámetros para usar en distintos sitios.
-;------------------------------------------------------------------
-(defvar cg-servidor "php -S localhost:4321 -t /almacenamiento/proyectos/yamm/"
-  "Para abrir servidor web")
-
