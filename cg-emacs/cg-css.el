1908595c25057f80d7c0243ab6ce5aee97193514
rjcordoba <rcordoba77@yahoo.es>
2021-07-11 19:32:15 +0200

    Añadido archivo para funcionalidades de CSS.

---
+++ cg-emacs/cg-css.el
@@ -0,0 +1,7 @@
+;------------------------------------------------------------------
+;    Archivo de configuración de Emacs - R. Córdoba García
+;    Utilidades para CSS
+;------------------------------------------------------------------
+
+(cg-configs-modo
+ :poner (("C-c s" . cg-servidor)))
